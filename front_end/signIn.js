b8fe28ec6138739e3bef536b476fb4860b511512
Lakeram Narine <nar19002@byui.edu>
2021-09-27 09:39:50 -0600

    changes to redirect

--- front_end/signIn.js
+++ front_end/signIn.js
@@ -4,11 +4,36 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    var email = profile.getEmail()
+    var id = profile.getId()
+
+    const approvedUser = ["nol18003@byui.edu", "leonarine@gmail.com", "tnolasco54@gmail.com"]
+    // check with bro murdock on how to add a param to the index.html
+    // approvedUser.forEach(element => {
+    //     if (element == email) {
+    //         location.replace(`http://localhost:5500/Google-Sign-in/front_end/home.html`
+    //             // location.replace(`http://localhost:5500/Google-Sign-in/front_end/home.html#${id}`
+    //         )
+    //     }
+
+    // });
 }
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        console.log('User signed out.');
+
+        // console.log('User signed out.')
+        // location.replace(`http://localhost:5500/Google-Sign-in/front_end/index.html`)
+
+
     });
 }
+
+
+// approvedUser.forEach(element => {
+//     if (element == email) {
+//         location.replace("http://localhost:5500/Google-Sign-in/front_end/home.html")
+//     }
+
+// });
