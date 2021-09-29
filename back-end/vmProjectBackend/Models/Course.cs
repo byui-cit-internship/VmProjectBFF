a2d6213012f0575ad18ac9c68283a97345301e41
Lakeram Narine <nar19002@byui.edu>
2021-09-29 16:21:48 -0600

    added models to model folder

---
+++ back-end/vmProjectBackend/Models/Course.cs
@@ -0,0 +1,9 @@
+namespace vmProjectBackend.Models
+{
+    public class Course
+    {
+        public int courseCode {get;set;}
+        public int section_num {get;set;}
+        
+    }
+}
---
+++ back-end/vmProjectBackend/Models/User.cs
@@ -0,0 +1,16 @@
+namespace vmProjectBackend.Models
+{
+    public class User
+
+    {
+        // [Key]
+        public long UserId {get;set;}
+        public string firstName {get; set;}
+        public string lastName {get;set;}
+        public string email {get;set;}
+        public string userType {get;set;}
+    }
+
+
+    
+}
