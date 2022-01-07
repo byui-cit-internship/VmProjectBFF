fe496e5d2cc320c6ee4e6b2c7f7e514a1acc0268
Sean Murdock <murdockse@byui.edu>
2022-01-07 09:57:58 -0700

    Add DB Migration to startup

--- vmProjectBackend/Startup.cs
+++ vmProjectBackend/Startup.cs
@@ -98,5 +98,20 @@ namespace vmProjectBackend
                 endpoints.MapControllers();
             });
         }
+
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<ConstellationWebappContext>())
+                {
+                    context.Database.Migrate();
+                }
+            }
+            )
+
+        }
     }
 }
