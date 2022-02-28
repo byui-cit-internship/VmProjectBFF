4f347919702be150c364327fa3f5272345870499
David Martinez <dpmartinez4@byui.edu>
2022-02-28 14:39:30 -0700

    databaseupdate

--- vmProjectBackend/Models/VmSpecification.cs
+++ vmProjectBackend/Models/VmSpecification.cs
@@ -8,14 +8,25 @@ namespace vmProjectBackend.Models
         [Key]
         public Guid VmSpecification_id { get;set;}
         
-        public string name { get;set;}
+        public string spec_name { get;set;}
 
-        public string guest_OS { get;set;}
+        public string placement_datastore { get;set;}
 
-        public string datastore { get;set;}
+        public string placement_folder { get;set;}
 
-        public string folder { get;set;}
+        public string placement_resource_pool { get;set;}
 
-        public string resource_pool { get;set;}
+        public string memory_size_MiB { get;set;}
+
+        public string cpu_count { get;set;}
+
+        public string  cpu_cores_per_socket { get;set;}
+
+        public string cdroms_iso_file { get;set;}
+
+        public string disks_capacity { get;set;}
+
+        
     }
 }
+
