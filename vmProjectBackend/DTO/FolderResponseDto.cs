19201fab89e0a017ceb94976dc3afa29281dc69d
sarii96 <sarah-96325@hotmail.com>
2022-03-18 09:23:20 -0600

    folder endpoint done

--- vmProjectBackend/DTO/FolderResponseDto.cs
+++ vmProjectBackend/DTO/FolderResponseDto.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 namespace vmProjectBackend.DTO
 {
     public class FolderResponse {
 
-        List<Folder> value { get; set;};
+         public List<Folder> value { get; set;}
 
     }
 }
