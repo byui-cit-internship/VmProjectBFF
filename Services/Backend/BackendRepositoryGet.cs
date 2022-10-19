2a26a1b312a10232058535a9e92228eac72092cb
Madison Lutz <84884030+mlutz060@users.noreply.github.com>
2022-10-19 09:04:52 -0600

    M adi lutz (#49)
    * typo corrected
    
    * Service based request setup dependency injection
    
    * vm stuff
    
    * controller stuff for vms
    
    * vm stuff
    
    Co-authored-by: mlutz06 <madison.lutz@sentinelofsafety.com>
    Co-authored-by: mebenal <59780051+mebenal@users.noreply.github.com>

--- Services/Backend/BackendRepositoryGet.cs
+++ Services/Backend/BackendRepositoryGet.cs
@@ -1,4 +1,4 @@
-﻿using vmProjectBFF.DTO;
+using vmProjectBFF.DTO;
 using Newtonsoft.Json;
 using vmProjectBFF.Models;
 
@@ -32,6 +32,40 @@ namespace vmProjectBFF.Services
         {
             _lastResponse = _backendHttpClient.Get($"api/v2/User", new { sectionId = sectionId });
             return JsonConvert.DeserializeObject<List<User>>(_lastResponse.Response);
+        }
+        public dynamic getInstancesByUser(int userId)
+        {
+            _lastResponse = _backendHttpClient.Get("api/v2/VmInstance", new { userId = userId });
+            List<VmInstance> vmInstances = JsonConvert.DeserializeObject<List<VmInstance>>(_lastResponse.Response);
+            List<int> vmTemplateIds = (from instance in vmInstances
+                                       select instance.VmTemplateId).Distinct().ToList();
+            List<VmTemplate> vmTemplates = new();
+            foreach (int vmTemplateId in vmTemplateIds)
+            {
+                _lastResponse = _backendHttpClient.Get("api/v2/VmTemplate", new { vmTemplateId = vmTemplateId });
+                vmTemplates.Add(JsonConvert.DeserializeObject<VmTemplate>(_lastResponse.Response));
+            }
+            List<dynamic> courses = new();
+            foreach (int vmTemplateId in vmTemplateIds)
+            {
+                _lastResponse = _backendHttpClient.Get("api/v2/Course", new { vmTemplateId = vmTemplateId });
+                dynamic course = JsonConvert.DeserializeObject<dynamic>(_lastResponse.Response);
+                course.VmTemplateId = vmTemplateId;
+                courses.Add(course);
+            }
+            return (
+                (from vi in vmInstances
+                 join vt in vmTemplates
+                 on vi.VmTemplateId equals vt.VmTemplateId
+                 join c in courses
+                 on vt.VmTemplateId equals c.VmTemplateId
+                 select new
+                 {
+                     CourseCode = c.CourseCode,
+                     VmTemplateName = vt.VmTemplateName,
+                     VmInstanceExpireDate = vi.VmInstanceExpireDate
+                 }).ToList()
+            );
         }
     }
 }
