f7cbb70984b08cba36256b16e7bfd39d52d4efc3
Daniel De La Pena <dadelapema@gmail.com>
2022-10-20 16:23:58 -0600

    send email class

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,41 +1,46 @@
 using System.Net.Mail;
 using System.Net;
-using System.Text;
+using vmProjectBFF.Exceptions;
 
 namespace vmProjectBFF.Services
 {
     public class EmailClient
     {
-        
+        public void sendEmail(
+            string receiverEmail,
+            int code,
+            string emailHead,
+            string Subject        )
+        {
+            // var senderEmail = configuration.GetConnectionString("vimaEmail");
+            // var senderEmailPassword = configuration.GetConnectionString("vimaEmailPassword");
+            // var clientHost = configuration.GetConnectionString("emailClientHost");
+            var senderEmail = "fakeuser1995@outlook.com";
+            var senderEmailPassword = "fakepassword123";
+
+            string body = "The code is" + code.ToString();
+
+            MailMessage mail = new MailMessage();
+            mail.To.Add(receiverEmail);
+            mail.From = new MailAddress(senderEmail, emailHead);
+            mail.Subject = Subject;
+            mail.Body = body;
+
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new NetworkCredential(senderEmail, senderEmailPassword);
+            client.Port = 587;
+            client.Host = "smtp.outlook.com";
+            client.EnableSsl = true;
+            
+            try
+            {
+                client.Send(mail);
+            }
+            catch (BffHttpException be)
+            {
+                Console.WriteLine(be);
+            }
+        }
     }
+
 }
-
-// MailMessage mail = new MailMessage();
-// mail.To.Add("dadelapema@gmail.com");
-// mail.From = new MailAddress("fakeuser1995@outlook.com", "Email head", Encoding.UTF8);
-// mail.Subject = "This mail is send from asp.net application";
-// //mail.SubjectEncoding = System.Text.Encoding.UTF8;
-// mail.Body = "This is Email Body Text";
-// //mail.BodyEncoding = System.Text.Encoding.UTF8;
-// //mail.IsBodyHtml = true;
-// mail.Priority = MailPriority.High;
-
-// SmtpClient client = new SmtpClient();
-// client.Credentials = new NetworkCredential("fakeuser1995@outlook.com", "fakepassword123");
-// client.Port = 587;
-// client.Host = "smtp.outlook.com";
-// client.EnableSsl = true;
-// try
-// {
-//     client.Send(mail);
-// }
-// catch (Exception ex)
-// {
-//     Exception ex2 = ex;
-//     string errorMessage = string.Empty;
-//     while (ex2 != null)
-//     {
-//         errorMessage += ex2.ToString();
-//         ex2 = ex2.InnerException;
-//     }
-// }
