cc71e3c6135e950e4646e56b60f2a381b5b6d9bc
ooioiooGT <gilber1002@byui.edu>
2022-11-14 10:35:12 -0700

    optional true

--- Startup.cs
+++ Startup.cs
@@ -14,7 +14,7 @@ namespace VmProjectBFF
         {
             IConfigurationBuilder builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true , reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
 
