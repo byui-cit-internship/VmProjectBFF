d29689abbfca3a07e282582b23576313d9dd0609
Punyanith Tangsiriruangrit <93311117+punyanith@users.noreply.github.com>
2023-04-17 06:14:59 -0600

    NOT DONE trying to fix create vm (#148)

--- DTO/CourseListByUserDTO.cs
+++ DTO/CourseListByUserDTO.cs
@@ -5,17 +5,16 @@
         public int canvasSectionId { get; set; }
         public int sectionId { get; set; }
         public string sectionName { get; set; }
-        public int enrollmentId { get; set; }
+        public int sectionRoleId { get; set; }
         public string studentFullName { get; set; }
         public string vcenterTemplateId { get; set; }
         public string LibraryVCenterId {get; set; } 
-        public int userSectionRoleId {get; set; }
-        public CourseListByUserDTO(int canvasSectionId, int sectionId, string sectionName, int enrollmentId, string studentFullName, string vcenterTemplateId, string LibraryVCenterId)
+        public CourseListByUserDTO(int canvasSectionId, int sectionId, string sectionName, int sectionRoleId, string studentFullName, string vcenterTemplateId, string LibraryVCenterId)
         {
             this.canvasSectionId = canvasSectionId;
             this.sectionId = sectionId;
             this.sectionName = sectionName;
-            this.enrollmentId = enrollmentId;
+            this.sectionRoleId = sectionRoleId;
             this.studentFullName = studentFullName;
             this.vcenterTemplateId = vcenterTemplateId;
             this.LibraryVCenterId = LibraryVCenterId;
